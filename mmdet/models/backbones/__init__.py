7c5c71fd25d285c49d1c283316415b94f79bb572
Sheng Xu <50565423+SteveTsui@users.noreply.github.com>
2022-07-20 14:37:30 +0800

    Add files via upload

---
+++ mmdet/models/backbones/__init__.py
@@ -0,0 +1,5 @@
+from .hrnet import HRNet
+from .resnet import ResNet, ResNetV1d
+from .resnext import ResNeXt
+from .resnet_react import ResNet_REACT
+__all__ = ['ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet','ResNet_REACT']
---
+++ mmdet/models/necks/__init__.py
@@ -0,0 +1,11 @@
+from .bfp import BFP
+from .fpn import FPN
+from .fpn_react_3x3 import FPN_REACT_3x3
+
+from .fpn_carafe import FPN_CARAFE
+from .hrfpn import HRFPN
+from .nas_fpn import NASFPN
+from .pafpn import PAFPN
+
+
+__all__ = ['FPN','FPN_REACT_3x3', 'BFP', 'HRFPN', 'NASFPN', 'FPN_CARAFE', 'PAFPN']

5dd15290fde351947f2036bbf996bd8e4f61473d
Sheng Xu <50565423+SteveTsui@users.noreply.github.com>
2023-04-03 14:49:40 +0800

    Delete mmdet/multi_distillation directory

--- mmdet/multi_distillation/__init__.py
+++
@@ -1,7 +0,0 @@
-from mmcv.runner import HOOKS, Hook
-from .detector_entropy import train_detector_entropy
-from .entro_loss import entro_beta_loss
-from .entropy_loss import entropy_loss
-
-
-__all__ = ['HOOKS', 'Hook', 'entro_beta_loss', 'train_detector_entropy', 'entropy_loss']
